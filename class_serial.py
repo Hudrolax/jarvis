9d57549a0ede1a4bab2c27969a22220a2770280b
Hudrolax <hudro795@gmail.com>
2020-04-27 17:06:36 +0300

    bugfix

--- class_serial.py
+++ class_serial.py
@@ -1,83 +1,26 @@
-from serial import Serial
-
+from serial import Serial, SerialException
+from serial.serialjava import *
 
 class JarvisSerial(Serial):
-    """\
-       Serial port base class. Provides __init__ function and properties to
-       get/set port settings.
-       """
-    PARITY_NONE, PARITY_EVEN, PARITY_ODD, PARITY_MARK, PARITY_SPACE = 'N', 'E', 'O', 'M', 'S'
-    STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO = (1, 1.5, 2)
-    FIVEBITS, SIXBITS, SEVENBITS, EIGHTBITS = (5, 6, 7, 8)
-
-    # default values, may be overridden in subclasses that do not support all values
-    BAUDRATES = (50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800,
-                 9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000,
-                 576000, 921600, 1000000, 1152000, 1500000, 2000000, 2500000,
-                 3000000, 3500000, 4000000)
-    BYTESIZES = (FIVEBITS, SIXBITS, SEVENBITS, EIGHTBITS)
-    PARITIES = (PARITY_NONE, PARITY_EVEN, PARITY_ODD, PARITY_MARK, PARITY_SPACE)
-    STOPBITS = (STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO)
-
-    def __init__(self,
-                 port=None,
-                 baudrate=9600,
-                 bytesize=EIGHTBITS,
-                 parity=PARITY_NONE,
-                 stopbits=STOPBITS_ONE,
-                 timeout=None,
-                 xonxoff=False,
-                 rtscts=False,
-                 write_timeout=None,
-                 dsrdtr=False,
-                 inter_byte_timeout=None,
-                 exclusive=None,
-                 **kwargs):
+     def open(self):
         """\
-        Initialize comm port object. If a "port" is given, then the port will be
-        opened immediately. Otherwise a Serial port object in closed state
-        is returned.
+        Open port with current settings. This may throw a SerialException
+        if the port cannot be opened.
         """
-
-        self.is_open = False
-        self.portstr = None
-        self.name = None
-        # correct values are assigned below through properties
-        self._port = None
-        self._baudrate = None
-        self._bytesize = None
-        self._parity = None
-        self._stopbits = None
-        self._timeout = None
-        self._write_timeout = None
-        self._xonxoff = None
-        self._rtscts = None
-        self._dsrdtr = None
-        self._inter_byte_timeout = None
-        self._rs485_mode = None  # disabled by default
-        self._rts_state = True
-        self._dtr_state = True
-        self._break_state = False
-        self._exclusive = None
-
-        # assign values using get/set methods using the properties feature
-        self.port = port
-        self.baudrate = baudrate
-        self.bytesize = bytesize
-        self.parity = parity
-        self.stopbits = stopbits
-        self.timeout = timeout
-        self.write_timeout = write_timeout
-        self.xonxoff = xonxoff
-        self.rtscts = rtscts
-        self.dsrdtr = dsrdtr
-        self.inter_byte_timeout = inter_byte_timeout
-        self.exclusive = exclusive
-
-        # watch for backward compatible kwargs
-        if 'writeTimeout' in kwargs:
-            self.write_timeout = kwargs.pop('writeTimeout')
-        if 'interCharTimeout' in kwargs:
-            self.inter_byte_timeout = kwargs.pop('interCharTimeout')
-        if kwargs:
-            raise ValueError('unexpected keyword arguments: {!r}'.format(kwargs))
+        if self._port is None:
+            raise SerialException("Port must be configured before it can be used.")
+        if self.is_open:
+            raise SerialException("Port is already open.")
+        if type(self._port) == type(''):      # strings are taken directly
+            portId = comm.CommPortIdentifier.getPortIdentifier(self._port)
+        else:
+            portId = comm.CommPortIdentifier.getPortIdentifier(device(self._port))     # numbers are transformed to a comport id obj
+        try:
+            self.sPort = portId.open("python serial module", 10)
+        except Exception as msg:
+            self.sPort = None
+            raise SerialException("Could not open port: %s" % msg)
+        # self._reconfigurePort()
+        self._instream = self.sPort.getInputStream()
+        self._outstream = self.sPort.getOutputStream()
+        self.is_open = True
